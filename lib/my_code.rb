951172e4d4d470ef0937a2496da5b891169afbaf
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 13:33:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,9 +1,9 @@
 # Your Code Here
-def map(source_array)
-  yield(source_array)
+def map(array)
+  new = array
 end
 
-map([]) {|b| b * -1}
+map(array) {|b| b * -1}
 
 def reduce(source_array, optional_value = 0)
 end
