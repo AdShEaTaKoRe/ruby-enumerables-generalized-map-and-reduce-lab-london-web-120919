2cfc4a88ad2ae11b0b0121974434acb93cbe9965
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 13:36:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,10 @@
 # Your Code Here
 def map(array)
   new = []
-  yield
+  yield(new)
 end
 
-map([1,2,3]){puts |b| b * -1}
+map(array){puts |b| b * -1}
 
 def reduce(source_array, optional_value = 0)
 end
