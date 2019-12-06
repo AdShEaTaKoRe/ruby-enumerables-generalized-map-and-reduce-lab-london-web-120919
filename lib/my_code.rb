38c0be3f015d6f77cb1cb8d2e6be8d63e212a41b
Tommy Berman <tomy.berman1@gmail.com>
2019-12-06 13:59:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map(array)
   end
   new
 end
+
+def reduced (array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while i < array.length
+  total += yield (array[i])
+  i += 1
+  end
+  tota
+end
